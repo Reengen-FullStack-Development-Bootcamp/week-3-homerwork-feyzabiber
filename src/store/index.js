2d088064f4de5a92ba2d7befdf910004c0c60a66
FeyzaBiber <feyzabiber@gmail.com>
2021-11-24 03:29:33 +0300

    Update CandleChart, Info, Add TimeSeries Endpoints

--- src/store/index.js
+++ src/store/index.js
@@ -1,58 +1,41 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
+import axios from "@/plugins/AxiosInstance.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     isAdmin: false,
-    title: 'Company',
-    number: 0,
-    apiURL: 'https://imdb8.p.rapidapi.com/',
-    headers: {
-      'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
-      'x-rapidapi-key': 'c942ed61e6mshc63b352f1b33912p19fb42jsn19568bc63c83' //api kodu
-    },
-    searchResults: [],
-    favMovies: [],
-    movieDetails: [],
-    actorDetails: [],
+    symbols: [],
   },
   mutations: {
     SET_IS_ADMIN(state, payload){
       state.isAdmin = payload
     },
-
-    deleteMatchedCompanies(state) {
-      state.matchedCompanies = [];
+    SET_SYMBOLS(state, payload) {
+      state.symbols = payload
     },
     
   },
   actions: {
-    searchCompany(context, data) {
-      axios
-        .get(`${context.state.api_endpoint}/query`, {
-          params: {
-            keywords: data.keywords,
-            function: "SYMBOL_SEARCH",
-            datatype: "json",
-          },
-          headers: {
-            "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
-            "x-rapidapi-key":
-              "c942ed61e6mshc63b352f1b33912p19fb42jsn19568bc63c83",
-          },
+    getSymbols(context, payload) {
+      return axios.get("/query", {
+        params: {
+          function: "SYMBOL_SEARCH",
+          keywords: payload,
+          datatype: 'json'
+        }
+      })
+        .then((resp) => {
+          context.commit("SET_SYMBOLS", resp.data.bestMatches)
         })
-        .then((response) => {
-          context.commit("setMatchedCompanies", response.data.bestMatches);
-          console.log(response);
-        });
-     },
+        .catch(err => console.log(err))
+    }
+
   },
 
   getters: {
-
   },
   modules: {},
 });
